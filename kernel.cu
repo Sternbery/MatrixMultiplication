d311bf58fdcf4ea1ef41c7f9c6595ee305aa5975
Madhav Kumar <madhavrkumar@gmail.com>
2019-06-17 09:07:14 -0400

    started using GPU to do multiplication and added basic profiling. math might be wrong on effective bandwidth. removed printing temporarily

--- kernel.cu
+++ kernel.cu
@@ -8,45 +8,26 @@
 #include <math.h>
 #include <iomanip>
 
-#define X 4
+#define X 1
 #define Y 4
-#define Z 1
-#define N 5
+#define Z 4
+#define N ((1<<20))
 
+__global__
 void multAll(float matrixListXY[], float matrixListYZ[]);
+__device__
 void matrixMult(float a[], float b[], int q);
-void printMatrixMult(float a[], float b[], float c[]);
 
-int main(void) {
-
-
-	dim3 matrixDim(X, Y, Z);
-
-	srand(2);
-
-	float matrixListXY[N * X * Y];
-	float matrixListYZ[N * Y * Z];
+//__device__
+//void printMatrixMult(float a[], float b[], float c[]);
 
 
-	for (int i = 0; i < N * X * Y; ++i) {
-		matrixListXY[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-	}
 
-	for (int i = 0; i < N * Y * Z; ++i) {
-		matrixListYZ[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-	}
-
-	multAll(matrixListXY, matrixListYZ);
-
-
-	return 0;
-}
-
-
+__global__
 void multAll(float matrixListXY[], float matrixListYZ[]) {
 
-	int index = 0;//blockIdx.x * blockDim.x + threadIdx.x;
-	int stride = 1;//blockDim.x * gridDim.x;
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	int stride = blockDim.x * gridDim.x;
 
 	//matrix a size is n*x*y
 	//matrix b size is n*y*z
@@ -57,10 +38,12 @@ void multAll(float matrixListXY[], float matrixListYZ[]) {
 	}
 }
 
+__device__
 void matrixMult(float a[], float b[], int q) {
 
 
-	float mult[X * Z];
+	float* mult = (float*)malloc(X * Z*sizeof(float));
+	
 	for (int i = 0; i < X; ++i) {
 		for (int j = 0; j < Z; ++j)
 		{
@@ -74,35 +57,39 @@ void matrixMult(float a[], float b[], int q) {
 				//this may not dereference correctly
 				//std::cout<<"a is of type: "<<typeid(q*x*y+i*y+j).name()<<std::endl;
 				//std::cout << a[q * X * Y + k * Y + i] << " * " << b[q * Y * Z + k * Z + j] << std::endl;
-				mult[j * X + i] += a[q * X * Y + k * Y + i] * b[q * Y * Z + k * Z + j];
+				mult[j * X + i] += a[q * X * Y + i * Y + k] * b[q * Y * Z + k * Z + j];
 			}
-			std::cout << std::endl;
 		}
 	}
 
 
-	if (false)
-		return;
+	//if (true)
+	//	return;
 
-	float ap[X * Y];
-	float bp[Y * Z];
+	//float* ap, * bp;
+	//cudaMallocManaged(&ap, X * Y * sizeof(float));
+	//cudaMallocManaged(&bp, Y * Z * sizeof(float));
 
-	for (int i = 0; i < X; ++i) {
-		for (int j = 0; j < Y; ++j)
-		{
-			ap[j * X + i] = a[q * X * Y + i * Y + j];
+	//for (int i = 0; i < X; ++i) {
+	//	for (int j = 0; j < Y; ++j)
+	//	{
+	//		ap[j * X + i] = a[q * X * Y + i * Y + j];
 
-		}
-	}
+	//	}
+	//}
 
-	for (int i = 0; i < Y; ++i) {
-		for (int j = 0; j < Z; ++j)
-		{
-			bp[j * Y + i] = b[q * Y * Z + i * Z + j];
-		}
-	}
+	//for (int i = 0; i < Y; ++i) {
+	//	for (int j = 0; j < Z; ++j)
+	//	{
+	//		bp[j * Y + i] = b[q * Y * Z + i * Y + j];
+	//	}
+	//}
 
-	printMatrixMult(ap, bp, mult);
+	//printMatrixMult(ap, bp, mult);
+
+	free(mult);
+	//cudaFree(ap);
+	//cudaFree(bp);
 }
 
 static inline int MAX(int a, int b) {
@@ -111,51 +98,102 @@ static inline int MAX(int a, int b) {
 	}
 	return b;
 }
+//__device__
+//void printmatrixmult(float a[], float b[], float c[]) {
+//	print one line
+//
+//	std::cout << std::setprecision(3);
+//	std::cout << std::fixed;
+//
+//	int maxi = max(z, max(x, y));
+//	for (int line = 0; line < maxi; ++line) {
+//
+//		matrix a
+//		if (line < y) {
+//			std::cout << "[";
+//			for (int i = 0; i < x; ++i)
+//				std::cout << " " << a[line * x + i] << ",";
+//			std::cout << "]";
+//		}
+//
+//		if (line == 0)
+//			std::cout << "*";
+//		else
+//			std::cout << " ";
+//
+//		matrix b
+//		if (line < z) {
+//			std::cout << "[";
+//			for (int i = 0; i < y; ++i)
+//				std::cout << " " << b[line * y + i] << ",";
+//			std::cout << "]";
+//		}
+//
+//		if (line == 0)
+//			std::cout << "=";
+//		else
+//			std::cout << " ";
+//
+//		matrix c
+//		if (line < z) {
+//			std::cout << "[";
+//			for (int i = 0; i < x; ++i)
+//				std::cout << " " << c[line * x + i] << ",";
+//			std::cout << "]";
+//		}
+//		std::cout << "\n";
+//	}
+//	std::cout << "\n";
+//}
 
-void printMatrixMult(float a[], float b[], float c[]) {
-	//print one line
+int main(void) {
 
-	std::cout << std::setprecision(3);
-	std::cout << std::fixed;
 
-	int maxi = MAX(Z, MAX(X, Y));
-	for (int line = 0; line < maxi; ++line) {
+	dim3 matrixDim(X, Y, Z);
 
-		//matrix a
-		if (line < Y) {
-			std::cout << "[";
-			for (int i = 0; i < X; ++i)
-				std::cout << " " << a[line * X + i] << ",";
-			std::cout << "]";
-		}
+	srand(2);
 
-		if (line == 0)
-			std::cout << "*";
-		else
-			std::cout << " ";
+	float* matrixListXY, * matrixListYZ, *XY, *YZ;
 
-		//matrix b
-		if (line < Z) {
-			std::cout << "[";
-			for (int i = 0; i < Y; ++i)
-				std::cout << " " << b[line * Y + i] << ",";
-			std::cout << "]";
-		}
+	XY = (float*)malloc(N * X * Y * sizeof(float));
+	YZ = (float*)malloc(N * Y * Z * sizeof(float));
 
-		if (line == 0)
-			std::cout << "=";
-		else
-			std::cout << " ";
+	cudaMallocManaged(&matrixListXY, N * X * Y * sizeof(float));
+	cudaMallocManaged(&matrixListYZ, N * Y * Z * sizeof(float));
 
-		//matrix c
-		if (line < Z) {
-			std::cout << "[";
-			for (int i = 0; i < X; ++i)
-				std::cout << " " << c[line * X + i] << ",";
-			std::cout << "]";
-		}
-		std::cout << "\n";
+
+
+	for (int i = 0; i < N * X * Y; ++i) {
+		XY[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 	}
-	std::cout << "\n";
+
+	for (int i = 0; i < N * Y * Z; ++i) {
+		YZ[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	}
+
+	cudaEvent_t start, stop;
+
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	cudaMemcpy(matrixListXY, XY, N * X * Y * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(matrixListYZ, YZ, N * Y * Z * sizeof(float), cudaMemcpyHostToDevice);
+
+	cudaEventRecord(start);
+	multAll <<< (N+511)/512, 512 >> > (matrixListXY, matrixListYZ);
+	cudaEventRecord(stop);
+
+	cudaEventSynchronize(stop);
+	float milliseconds = 0.0f;
+	cudaEventElapsedTime(&milliseconds, start, stop);
+
+	printf("Effective Bandwidth (GB/s): %f", ((N * 4 * X * Y )+ (N*4*Y*Z)) / milliseconds / 1e6);
+	cudaFree(matrixListXY);
+	cudaFree(matrixListYZ);
+
+	free(XY);
+	free(YZ);
+	return 0;
 }
 
+ 
