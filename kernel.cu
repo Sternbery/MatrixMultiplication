4d569cac055b6c77f489c866432f1542ccf403d0
MadhavRKumar <MadhavRKumar@gmail.com>
2019-06-20 14:43:00 -0400

    updated cuda code to have option of single matrix multiplication, removed initilization of result to cpu code

--- kernel.cu
+++ kernel.cu
@@ -11,85 +11,44 @@
 #define X 1
 #define Y 4
 #define Z 4
-#define N ((1<<20))
+#define N ((5))
 
 __global__
-void multAll(float matrixListXY[], float matrixListYZ[]);
+void multAll(float matrixListXY[], float matrixListYZ[], float matrixListXZ[]);
 __device__
-void matrixMult(float a[], float b[], int q);
+void matrixMult(float xy[], float yz[], float xz[], int q);
 
-//__device__
-//void printMatrixMult(float a[], float b[], float c[]);
+__global__
+void matrixMultSingle(float xy[], float yz[], float xz[], int q );
+
+void printMatrixMult(float a[], float b[], float c[], int x, int y, int z);
 
 
 
 __global__
-void multAll(float matrixListXY[], float matrixListYZ[]) {
+void multAll(float matrixListXY[], float matrixListYZ[], float matrixListXZ[]) {
 
-	int index = blockIdx.x * blockDim.x + threadIdx.x;
-	int stride = blockDim.x * gridDim.x;
+	int index = 0;//blockIdx.x * blockDim.x + threadIdx.x;
+	int stride = 1;//blockDim.x * gridDim.x;
 
 	//matrix a size is n*x*y
 	//matrix b size is n*y*z
-	
+
 
 	for (int i = index; i < N; i += stride) {
-		matrixMult(matrixListXY, matrixListYZ, i);
+		matrixMult(matrixListXY, matrixListYZ, matrixListXZ, i );
 	}
 }
 
 __device__
-void matrixMult(float a[], float b[], int q) {
-
-
-	float* mult = (float*)malloc(X * Z*sizeof(float));
-	
-	for (int i = 0; i < X; ++i) {
-		for (int j = 0; j < Z; ++j)
-		{
-			mult[j * X + i] = 0;
-		}
-	}
-
+void matrixMult(float xy[], float yz[], float xz[], int q) {
 	for (int i = 0; i < X; ++i) {
 		for (int j = 0; j < Z; ++j) {
 			for (int k = 0; k < Y; ++k) {
-				//this may not dereference correctly
-				//std::cout<<"a is of type: "<<typeid(q*x*y+i*y+j).name()<<std::endl;
-				//std::cout << a[q * X * Y + k * Y + i] << " * " << b[q * Y * Z + k * Z + j] << std::endl;
-				mult[j * X + i] += a[q * X * Y + i * Y + k] * b[q * Y * Z + k * Z + j];
+				xz[q*X*Z + i*Y + j] += xy[q*X*Y + i*Y + k] * yz[q*Y*Z + k*Z + j];
 			}
 		}
 	}
-
-
-	//if (true)
-	//	return;
-
-	//float* ap, * bp;
-	//cudaMallocManaged(&ap, X * Y * sizeof(float));
-	//cudaMallocManaged(&bp, Y * Z * sizeof(float));
-
-	//for (int i = 0; i < X; ++i) {
-	//	for (int j = 0; j < Y; ++j)
-	//	{
-	//		ap[j * X + i] = a[q * X * Y + i * Y + j];
-
-	//	}
-	//}
-
-	//for (int i = 0; i < Y; ++i) {
-	//	for (int j = 0; j < Z; ++j)
-	//	{
-	//		bp[j * Y + i] = b[q * Y * Z + i * Y + j];
-	//	}
-	//}
-
-	//printMatrixMult(ap, bp, mult);
-
-	free(mult);
-	//cudaFree(ap);
-	//cudaFree(bp);
 }
 
 static inline int MAX(int a, int b) {
@@ -98,69 +57,78 @@ static inline int MAX(int a, int b) {
 	}
 	return b;
 }
-//__device__
-//void printmatrixmult(float a[], float b[], float c[]) {
-//	print one line
-//
-//	std::cout << std::setprecision(3);
-//	std::cout << std::fixed;
-//
-//	int maxi = max(z, max(x, y));
-//	for (int line = 0; line < maxi; ++line) {
-//
-//		matrix a
-//		if (line < y) {
-//			std::cout << "[";
-//			for (int i = 0; i < x; ++i)
-//				std::cout << " " << a[line * x + i] << ",";
-//			std::cout << "]";
-//		}
-//
-//		if (line == 0)
-//			std::cout << "*";
-//		else
-//			std::cout << " ";
-//
-//		matrix b
-//		if (line < z) {
-//			std::cout << "[";
-//			for (int i = 0; i < y; ++i)
-//				std::cout << " " << b[line * y + i] << ",";
-//			std::cout << "]";
-//		}
-//
-//		if (line == 0)
-//			std::cout << "=";
-//		else
-//			std::cout << " ";
-//
-//		matrix c
-//		if (line < z) {
-//			std::cout << "[";
-//			for (int i = 0; i < x; ++i)
-//				std::cout << " " << c[line * x + i] << ",";
-//			std::cout << "]";
-//		}
-//		std::cout << "\n";
-//	}
-//	std::cout << "\n";
-//}
+
+void printMatrixMult(float a[], float b[], float c[], int x, int y, int z){
+	//print one line
+
+	//std::cout<<std::setprecision(3);
+	//std::cout<<std::fixed;
+
+	int maxi = MAX(z,MAX(x,y));
+	for(int line=0;line<maxi;++line){
+
+		//matrix a
+		if(line<y){
+			printf("[");
+			for(int i=0;i<x;++i)
+				printf(" %f,",a[line*x+i]);
+			printf("]");
+		}
+
+		if(line==0)
+			printf("*");
+		else
+			printf(" ");
+
+		//matrix b
+		if(line<z){
+			printf("[");
+			for(int i=0;i<y;++i)
+				printf(" %f,",b[line*y+i]);
+			printf("]");
+		}
+
+		if(line==0)
+			printf("=");
+		else
+			printf(" ");
+
+		//matrix c
+		if( line<z){
+			printf("[");
+			for(int i=0;i<x;++i)
+				printf(" %f,",c[line*x+i]);
+			printf("]");
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+__global__
+// multi threaded matrix multiplication of a single matrix
+void matrixMultSingle(float xy[], float yz[], float xz[], int q ) {
+	int index = 0;//blockIdx.x * blockDim.x + threadIdx.x;
+	int stride = 1;//blockDim.x * gridDim.x;
+
+	for(int a = index; a < X*Y*Z; a+=stride){
+		xz[q*X*Z + a/Y/Z*Y + (a/Z) % Y] += xy[q*X*Y + a/Y/Z*Y + a%Z] * yz[q*Y*Z + a%Z*Z + (a/Z) % Y];
+	}
+}
+
 
 int main(void) {
-
-
-	dim3 matrixDim(X, Y, Z);
-
 	srand(2);
 
-	float* matrixListXY, * matrixListYZ, *XY, *YZ;
+	float* matrixListXY, * matrixListYZ, *matrixListXZ, *XY, *YZ, *XZ;
 
 	XY = (float*)malloc(N * X * Y * sizeof(float));
 	YZ = (float*)malloc(N * Y * Z * sizeof(float));
+	XZ = (float*)malloc(N * X * Z * sizeof(float));
 
 	cudaMallocManaged(&matrixListXY, N * X * Y * sizeof(float));
 	cudaMallocManaged(&matrixListYZ, N * Y * Z * sizeof(float));
-
+	cudaMallocManaged(&matrixListXZ, N * X * Z * sizeof(float));
 
 
 	for (int i = 0; i < N * X * Y; ++i) {
@@ -171,6 +139,11 @@ int main(void) {
 		YZ[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 	}
 
+
+	for (int i = 0; i < N * X * Z; ++i) {
+		XZ[i] = (float) 0;
+	}
+
 	cudaEvent_t start, stop;
 
 	cudaEventCreate(&start);
@@ -178,21 +151,28 @@ int main(void) {
 
 	cudaMemcpy(matrixListXY, XY, N * X * Y * sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(matrixListYZ, YZ, N * Y * Z * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(matrixListXZ, XZ, N * X * Z * sizeof(float), cudaMemcpyHostToDevice);
 
 	cudaEventRecord(start);
-	multAll <<< (N+511)/512, 512 >> > (matrixListXY, matrixListYZ);
+	int THREADS = 256;
+	for(int i = 0 ; i < 1; i++) {
+		multAll <<< (N+THREADS - 1)/THREADS, THREADS >> > (matrixListXY, matrixListYZ, matrixListXZ);
+	}
 	cudaEventRecord(stop);
 
 	cudaEventSynchronize(stop);
-	float milliseconds = 0.0f;
-	cudaEventElapsedTime(&milliseconds, start, stop);
 
-	printf("Effective Bandwidth (GB/s): %f", ((N * 4 * X * Y )+ (N*4*Y*Z)) / milliseconds / 1e6);
+	cudaMemcpy(XZ, matrixListXZ, N * X * Z * sizeof(float), cudaMemcpyDeviceToHost);
+
+
+	printMatrixMult(XY, YZ, XZ, X, Y, Z);
 	cudaFree(matrixListXY);
 	cudaFree(matrixListYZ);
+	cudaFree(matrixListXZ);
 
 	free(XY);
 	free(YZ);
+	free(XZ);
 	return 0;
 }
 
